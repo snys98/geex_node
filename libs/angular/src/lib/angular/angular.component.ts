b4bf14c2acb4bb389bf398b60299c413e78f4f24
Lulus <snys98@outlook.com>
2023-12-20 13:29:18 +0800

    structure init

---
+++ libs/angular/src/lib/angular/angular.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'lib-angular',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './angular.component.html',
+  styleUrl: './angular.component.css',
+})
+export class AngularComponent {}
--- libs/core/jest.config.ts
+++ libs/core/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: '@geex/core',
+  displayName: 'core',
   preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
